d5909977639bedcedce32a66d8827a21f01f6b78
WassiW80 <sayedwaseeyat111@gmail.com>
2020-08-10 21:42:21 +0530

    ADDED: Main class.

--- src/com/breakbreaker/Main.java
+++ src/com/breakbreaker/Main.java
@@ -4,6 +4,13 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        JFrame jFrame = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        jFrame.setBounds(10, 10, 700, 600);
+        jFrame.setTitle("Break Breaker");
+        jFrame.setResizable(false);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+        jFrame.add(gamePlay);
     }
 }
