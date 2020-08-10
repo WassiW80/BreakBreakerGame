fe6b2cda8e8ada4a437e641eaec46c26e44444bf
WassiW80 <sayedwaseeyat111@gmail.com>
2020-08-10 21:44:40 +0530

    ADDED: Ball And Pedal Display.

--- src/com/breakbreaker/GamePlay.java
+++ src/com/breakbreaker/GamePlay.java
@@ -1,12 +1,54 @@
 package com.breakbreaker;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class GamePlay extends JPanel implements KeyListener, ActionListener {
+    private boolean play = false;
+    private int score = 0;
+
+    private int totalBrick = 21;
+
+    private Timer timer;
+    private int delay = 8;
+
+    private int playerX = 310;
+
+    private int ballPosX = 120;
+    private int ballPosY = 350;
+    private int ballXDir = -1;
+    private int ballYDir = -2;
+
+    public GamePlay() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    public void paint(Graphics graphics) {
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(1, 1, 692, 592);
+
+        graphics.setColor(Color.YELLOW);
+        graphics.fillRect(0, 0, 3, 592);
+        graphics.fillRect(0, 0, 692, 3);
+        graphics.fillRect(691, 0, 3, 592);
+
+        graphics.setColor(Color.BLUE);
+        graphics.fillRect(playerX, 550, 100, 8);
+
+        graphics.setColor(Color.GREEN);
+        graphics.fillOval(ballPosX, ballPosY, 20, 20);
+
+        graphics.dispose();
+    }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -20,7 +62,29 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (playerX >= 600)
+                playerX = 600;
+            else
+                moveRight();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (playerX < 10)
+                playerX = 10;
+            else
+                moveLeft();
+        }
 
+    }
+
+    private void moveRight() {
+        play = true;
+        playerX += 20;
+    }
+
+    private void moveLeft() {
+        play = true;
+        playerX -= 20;
     }
 
     @Override
